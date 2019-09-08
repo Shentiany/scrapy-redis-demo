3fae9a1c8fd1450c0f92a5bc8370eb28dcf3fe81
shentiany <1796725346@qq.com>
2019-09-08 14:48:11 +0800

    First commit

---
+++ douban/douban/spiders/movie.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from items import DoubanItem
+
+
+class MovieSpider(scrapy.Spider):
+    name = 'movie'
+    allowed_domains = ['movie.douban.com']
+    start_urls = ['http://movie.douban.com/top250/']
+
+    def parse(self, response):
+        detail_urls = response.xpath('//div[@class="hd"]/a/@href').extract()
+        print(detail_urls)
+        for detail_url in detail_urls:
+            yield scrapy.Request(detail_url, callback=self.detail_info)
+        page = response.xpath('//span[@class="next"]/link/@href').extract_first()
+        if page is not None:
+            url = f'https://movie.douban.com/top250{page}'
+            yield scrapy.Request(url, callback=self.parse)
+
+    def detail_info(self, response):
+        item = DoubanItem()
+        name = response.xpath('//h1/span[1]/text()').extract()
+        director = response.xpath('//*[contains(text(), "导演")]/following-sibling::span[1]//text()').extract()
+        item['name'] = name
+        item['director'] = director
+        yield item
+
